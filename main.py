dcf841bd52fb5c6c75f471391f2d92b439bec778
RedouanKACIMI <redouan.kacimi@outlook.com>
2020-02-02 18:42:45 +0100

    decided for light Design, redesigned Home menu, added icons

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
+
 
 class IntroShot(Screen):
     pass
@@ -13,14 +15,18 @@ class IntroShot(Screen):
 class MenuShot(Screen):
     pass
 
+class MenShot(Screen):
+    pass
+
 
 class ShotManager(ScreenManager):
     pass
 
 
 kv = Builder.load_file("stilButton.kv")
+kv = Builder.load_file("stilLabel.kv")
 kv = Builder.load_file("stilMain.kv")
-Window.clearcolor = (.13,.13,.19,1)
+Window.clearcolor = (1, 1, 1, 1)
 
 
 
