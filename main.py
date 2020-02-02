6f7b3edaf74bd1dea4aa88e1d1b11ea3e4ced4be
RedouanKACIMI <redouan.kacimi@outlook.com>
2020-02-02 22:14:27 +0100

    Added the Sprechstunde Page -Openning time- and connected it to Mysql

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="ianua"
+)
+
+auge = mydb.cursor()
+auge.execute("SELECT * FROM sprechstd")
+sprechstden = auge.fetchall()
+
+
+# for x in sprechstden:
+#   print(x[2]+' bis '+x[3])
+
+
+
 
 
 class IntroShot(Screen):
@@ -15,16 +35,22 @@ class IntroShot(Screen):
 class MenuShot(Screen):
     pass
 
-class MenShot(Screen):
-    pass
+class SprechShot(Screen):
+    def sprechen(self):
+        sprch = ''
+        for x in sprechstden:
+            sprch += str(x[1])+' /  '+ x[2] + ' - '+ x[3]+'\n'
+        return sprch
 
 
 class ShotManager(ScreenManager):
     pass
 
 
+
 kv = Builder.load_file("stilButton.kv")
 kv = Builder.load_file("stilLabel.kv")
+kv = Builder.load_file("stilSprech.kv")
 kv = Builder.load_file("stilMain.kv")
 Window.clearcolor = (1, 1, 1, 1)
 
